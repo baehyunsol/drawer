e147e2cbf82dff2995ef862282aa970ed675c968
baehyunsol <baehyunsol@naver.com>
2022-02-12 11:21:49 +0900

    more precise font rendering

--- src/font.rs
+++ src/font.rs
@@ -9,12 +9,6 @@ use std::sync::Arc;
 use std::fs::File;
 use std::collections::HashMap;
 
-/*
-TODO
-
-make fonts more horizontally tight
-*/
-
 
 pub struct Font {
     size: usize,
@@ -25,7 +19,7 @@ pub struct Font {
     background: Option<Color>,
     outline: Option<Color>,
     underline: Option<Color>,
-    horizontal_margin: usize
+    letter_spacing: usize
 }
 
 
@@ -37,7 +31,7 @@ impl Font {
 
         let mut result = Font {
             font_kit_font, size, rendered_buffers: HashMap::new(),
-            color, background: None, outline: None, underline: None, horizontal_margin: size / 5,
+            color, background: None, outline: None, underline: None, letter_spacing: size / 4,
             gap: Buffer::new(0, 0)
         };
 
@@ -47,6 +41,11 @@ impl Font {
     pub fn init(&mut self) -> &mut Self {
         self.init_ascii();
         self.init_gap();
+        self
+    }
+
+    pub fn init_ext(&mut self) -> &mut Self {
+        self.rasterize_font(128, 512);
         self
     }
 
@@ -60,10 +59,30 @@ impl Font {
         self
     }
 
-    fn init_ascii(&mut self) {
+    pub fn set_letter_spacing(&mut self, letter_spacing: usize) -> &mut Self {
+        self.letter_spacing = letter_spacing;
+        self
+    }
 
-        for i in 32..127 {
-            self.rendered_buffers.insert(i, self.render_single_char(i));
+    fn init_ascii(&mut self) {
+        self.rasterize_font(32, 127);
+    }
+
+    fn rasterize_font(&mut self, from: u32, to: u32) {
+
+        for i in from..to + 1 {
+            match self.render_single_char(i) {
+                Ok(mut buff) => {
+
+                    buff = match get_raster_width(&self.font_kit_font, self.size, i) {
+                        Ok(w) => buff.crop(0, 0, w as i32, buff.height as i32),
+                        _ => buff
+                    };
+
+                    self.rendered_buffers.insert(i, buff);
+                }
+                _ => {}
+            }
         }
 
     }
@@ -72,7 +91,7 @@ impl Font {
 
         let (_, height) = self.get_size(&" ".to_string());
 
-        let mut result = Buffer::new(self.horizontal_margin, height);
+        let mut result = Buffer::new(self.letter_spacing, height);
 
         match self.background {
             Some(c) => {result.fill(c);}
@@ -104,7 +123,7 @@ impl Font {
             result.blit(curr_buf, curr_x as i32, 0);
             curr_x += curr_buf.width;
             result.blit(&self.gap, curr_x as i32, 0);
-            curr_x += self.horizontal_margin;
+            curr_x += self.letter_spacing;
         }
 
         result
@@ -121,68 +140,125 @@ impl Font {
                 Some(b) => {curr += b.width;}
             }
 
-            curr += self.horizontal_margin;
+            curr += self.letter_spacing;
         }
 
         if string.len() > 0 {
-            curr -= self.horizontal_margin;
+            curr -= self.letter_spacing;
         }
 
         (curr, get_font_height(self.size))
     }
 
+    fn render_single_char(&self, utf_code: u32) -> Result<Buffer, ()> {
 
-    fn render_single_char(&self, glyph: u32) -> Buffer {
-    
-        let glyph = match std::char::from_u32(glyph) {
-            None => '?',
-            Some(c) => c
-        };
-    
-        let invalid_glyph_id = self.font_kit_font.glyph_for_char('?').unwrap();
-    
         let (width, height) = (self.size, get_font_height(self.size));
-    
         let mut result = Buffer::new(width, height);
+
+        let glyph_id = get_glyph_id(&self.font_kit_font, utf_code)?;
 
         match self.background {
             Some(c) => {result.fill(c);}
             _ => {}
         }
-    
-        let glyph_id = match self.font_kit_font.glyph_for_char(glyph) {
-            Some(id) => id,
-            None => invalid_glyph_id
-        };
-    
-        let mut canvas = Canvas::new(Vector2I::new(width as i32, height as i32), Format::A8);
-    
-        self.font_kit_font.rasterize_glyph(
-            &mut canvas,
-            glyph_id,
-            self.size as f32,
-            Transform2F::from_translation(Vector2F::new(0.0, self.size as f32)),
-            HintingOptions::None,
-            RasterizationOptions::GrayscaleAa,
-        )
-        .unwrap();
-    
-        for x in 0..width {
-            for y in 0..height {
-                if canvas.pixels[y * width + x] != 0 {
-                    result.set_pixel(x, y, Color::rgb(255, 255, 255));
-                }
-            }
-        }
+
+        result = draw_font_buffer(
+            result,
+            &self.font_kit_font,
+            width, height, self.size,
+            glyph_id, self.color
+        );
 
         match self.underline {
             Some(c) => {result.draw_line(0, self.size as i32, self.size as i32, self.size as i32, self.size as i32 / 20 + 1, c);}
             _ => {}
         }
-    
-        result
+
+        Ok(result)
     }
 
+}
+
+
+fn get_raster_width(raw_font: &FKFont, size: usize, utf_code: u32) -> Result<usize, ()> {
+
+    if size == 0 {
+        return Ok(0);
+    }
+
+    if utf_code == 32 {
+        return Ok(size / 3 + 1)
+    }
+
+    let (width, height) = (size, get_font_height(size));
+    let mut result = Buffer::new(width, height);
+
+    let glyph_id = get_glyph_id(raw_font, utf_code)?;
+
+    result = draw_font_buffer(
+        result,
+        raw_font,
+        width, height, size,
+        glyph_id, Color::rgb(255, 255, 255)
+    );
+
+    let mut curr = width - 1;
+
+    while curr > 0 {
+
+        for y in 0..height {
+
+            if result.get_pixel(curr, y).a > 0 {
+                return Ok(curr);
+            }
+
+        }
+
+        curr -= 1;
+    }
+
+    Ok(0)
+}
+
+
+fn get_glyph_id(raw_font: &FKFont, utf_code: u32) -> Result<u32, ()> {
+
+    let character = match std::char::from_u32(utf_code) {
+        None => {return Err(())},
+        Some(c) => c
+    };
+
+    let glyph_id = match raw_font.glyph_for_char(character) {
+        Some(id) => id,
+        None => {return Err(())}
+    };
+
+    Ok(glyph_id)
+}
+
+
+fn draw_font_buffer(mut buffer: Buffer, raw_font: &FKFont, width: usize, height: usize, size: usize, glyph_id: u32, color: Color) -> Buffer {
+
+    let mut canvas = Canvas::new(Vector2I::new(width as i32, height as i32), Format::A8);
+
+    raw_font.rasterize_glyph(
+        &mut canvas,
+        glyph_id,
+        size as f32,
+        Transform2F::from_translation(Vector2F::new(0.0, size as f32)),
+        HintingOptions::None,
+        RasterizationOptions::GrayscaleAa,
+    ).unwrap();
+
+    for x in 0..width {
+        for y in 0..height {
+            if canvas.pixels[y * width + x] != 0 {
+                buffer.set_pixel(x, y, color);
+            }
+        }
+    }
+
+    buffer
 }
 
 
--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,9 @@ mod font;
 fn main() {
 
     let mut parent = buffer::Buffer::new(1280, 1280);
-    let mut f = font::Font::from_file("font.otf", 64, color::Color::rgb(255, 255, 255)).unwrap();
-    f.set_underline(Some(color::Color::rgb(122, 33, 11))).set_background_color(Some(color::Color::rgb(44, 44, 99))).init();
-    parent.blit(&f.render(&String::from("Hello World! 아이유 + 볼빨간사춘기")), 0, 0);
+    let mut f = font::Font::from_file("font.otf", 32, color::Color::rgb(255, 255, 255)).unwrap();
+    f.set_underline(Some(color::Color::rgb(255, 255, 255))).set_background_color(Some(color::Color::rgb(44, 44, 99))).init();
+    parent.blit(&f.render(&String::from("Hello World!!! 볼빨간사춘기")), 0, 0);
 
     let c = color::Color::rgba(44, 44, 199, 254);
     parent.draw_ellipse(640, 640, 8, 3, 32, c);
